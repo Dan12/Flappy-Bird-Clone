2997bfd205b6d5c26d640a767cb4b9bc7b5f53f6
Dan <dangarbage15@yahoo.com>
2015-10-08 13:36:01 -0500

    added blood

---
+++ Assets/scripts/BloodScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = BirdScript.bird.transform.position;
+		transform.eulerAngles = BirdScript.bird.transform.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = BirdScript.bird.transform.position;
+		transform.eulerAngles = BirdScript.bird.transform.eulerAngles;
+	}
+}
